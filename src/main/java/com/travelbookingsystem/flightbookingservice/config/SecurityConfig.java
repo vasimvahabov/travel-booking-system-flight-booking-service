b5b32d36e55f0350e364c453be223106c437e2c8
Vasim <vahabov.vasim@gmail.com>
2025-10-22 00:26:34 +0200

    feat: configure authentication

---
+++ src/main/java/com/travelbookingsystem/flightbookingservice/config/SecurityConfig.java
@@ -0,0 +1,27 @@
+package com.travelbookingsystem.flightbookingservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+import org.springframework.security.web.server.savedrequest.NoOpServerRequestCache;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http.authorizeExchange(exchange ->
+                        exchange.anyExchange().authenticated()
+                ).oauth2ResourceServer(resourceServer ->
+                        resourceServer.jwt(Customizer.withDefaults())
+                ).requestCache(requestCache ->
+                        requestCache.requestCache(NoOpServerRequestCache.getInstance())
+                ).csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .build();
+    }
+
+}
